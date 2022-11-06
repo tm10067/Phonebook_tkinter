6975bda96afe664f3d2edf465ee03b37db4f89af
Michael <you@example.com>
2022-11-06 23:39:01 +0300

    start

--- Controller.py
+++ Controller.py
@@ -1,39 +1,41 @@
 import Model
 import View
 
-
-
 def main_menu():
     while True:
-        print('\nГлавное меню:')
-        print('1. Добавить контакт')
-        print('2. Удалить контакт')
-        print('3. Изменить контакт')
-        print('8. Сохранить файл')
-        print('0. Выйти из программы')
+        print('\nТелефонный справочник - главное меню:')
+        print('0. Показать все контакты')
+        print('1. Открыть файл с контактами')
+        print('2. Записать файл с контактами')
+        print('3. Добавить контакт')
+        print('4. Изменить контакт')
+        print('5. Удалить контакт')
+        print('6. Поиск п контактам')
+        print('9. Выйти из программы')
         choice = int(input('Выберите пункт: '))
-        match (choice):
-            case 1:
-                add_contact()
-                print('\nКонтакт добавлен\n')
-            case 2:
-                remove_contact()
-                print('\nКонтакт удален\n')
-            case 3:
-                change_contact()
-            case 8:
-                save_file()
-                print('\nФайл сохранен!\n')
-            case 0:
-                break
+        if choice == 0:
+            View.printPhoneBook()
 
+        elif choice == 2:
+            save_file()
+            print('\nФайл сохранен!\n')
+        elif choice == 3:
+            add_contact()
+            print('\nКонтакт добавлен\n')
+        elif choice == 4:
+            change_contact()           
+        elif choice == 5:
+            remove_contact()
+            print('\nКонтакт удален\n')
+
+        elif choice == 9:
+            break
+        else:
+            print("'\nНеправильно набран номер!\n'")
 
 def start():
     open_file()
-    View.printPhoneBook()
     main_menu()
-
-
 
 def open_file():
     with open(Model.path, "r", encoding="UTF-8") as data:
@@ -59,15 +61,13 @@ def remove_contact():
     View.printPhoneBook()
 
 def change_contact():
-
     choice = int(input('Введите номер элемента для изменения: '))
     choice2 = int(input('Что изменяем (0-имя, 1-фамилия, 2-отчество, 3-телефон): '))
-
     contact = Model.phonebook.pop(choice).split(';')
     print(contact)
     contact[choice2] = input('Введите новое значение: ')
     print(contact)
-    Model.phonebook.insert(choice, ';'.join(contact))
+    Model.phonebook.insert(choice, '; '.join(contact))
     View.printPhoneBook()
 
 
--- Model.py
+++ Model.py
@@ -1,3 +1,3 @@
 phonebook = []
 
-path = 'phone.txt'
+path = 'PhoneBookConsole/phone.txt'
